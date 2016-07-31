5c6f9ab6a78b5efadfbb5c47f59ed9aae47ee3cb
Andrzej Adamczyk <akasei@wataha.net>
2016-07-31 23:07:39 +0000

    fix locale

--- software/bfi/locale/pl_PL.ASCII.asm
+++ software/bfi/locale/pl_PL.ASCII.asm
@@ -1,1 +1,3 @@
 text_hello	db	"Interpretator Brain Fuck!", VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_TERMINATOR
+text_missing_close_pair	db	"devport, zapomnialem o tym tekscie", VARIABLE_ASCII_CODE_ENTER
+text_missing_open_pair	db	"devport, zapomnialem o tym tekscie", VARIABLE_ASCII_CODE_ENTER
