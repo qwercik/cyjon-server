2002e348f7404e87804de16e74a703ed955d0559
Andrzej Adamczyk <akasei@wataha.net>
2016-07-31 23:14:02 +0000

    Update en_US.ASCII.asm

--- software/bfi/locale/en_US.ASCII.asm
+++ software/bfi/locale/en_US.ASCII.asm
@@ -1,3 +1,3 @@
-text_hello	db	"Brain Fuck Interpreter!", VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_TERMINATOR
-text_missing_close_pair	db	"devport, zapomnialem o tym tekscie", VARIABLE_ASCII_CODE_ENTER
-text_missing_open_pair	db	"devport, zapomnialem o tym tekscie", VARIABLE_ASCII_CODE_ENTER
+text_hello		db	"BrainFuck Interpreter!", VARIABLE_ASCII_CODE_ENTER, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_NEWLINE, VARIABLE_ASCII_CODE_TERMINATOR
+text_missing_close_pair	db	"devport, zapomniales o tym tekscie", VARIABLE_ASCII_CODE_ENTER
+text_missing_open_pair	db	"devport, zapomniales o tym tekscie", VARIABLE_ASCII_CODE_ENTER
