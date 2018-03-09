01ed22aac9f6b658550bed638c6ef78b731e5518
Andrzej Adamczyk <akasei@wataha.net>
2018-03-09 22:27:18 +0100

    zbędne

--- kernel/daemons/shell/data.asm
+++
@@ -1,7 +0,0 @@
-;===============================================================================
-; Copyright (C) 2013+ by Andrzej Adamczyk at Wataha.net
-;===============================================================================
-
-daemon_shell_prompt_nl					db	ASCII_NEW_LINE
-daemon_shell_prompt					db	"# "
-daemon_shell_prompt_end:
